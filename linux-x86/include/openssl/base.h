09f8e145421ccde80e0e15bfef0be417a7d26cbd
Chun-Hung Wu <chun-hung.wu@mediatek.com>
2022-06-22 15:17:33 +0800

    [ALPS06428903] abi monitor: update lastest toolchain
      Update latest abimonitor toolchain to fix abidw report
    error if different ko have same symbol name. (commit 65339b386)
    Details see: google bugID b/236074847
    
    Signed-off-by: Chun-Hung Wu <chun-hung.wu@mediatek.com>
    CR-Id: ALPS06428903
    Feature: Kernel SI Operation
    Change-Id: I42876a8426962f580ab30d6491cd44ef3263f1db

--- linux-x86/include/openssl/base.h
+++ linux-x86/include/openssl/base.h
@@ -195,7 +195,7 @@ extern "C" {
 // A consumer may use this symbol in the preprocessor to temporarily build
 // against multiple revisions of BoringSSL at the same time. It is not
 // recommended to do so for longer than is necessary.
-#define BORINGSSL_API_VERSION 16
+#define BORINGSSL_API_VERSION 17
 
 #if defined(BORINGSSL_SHARED_LIBRARY)
 
@@ -402,6 +402,7 @@ typedef struct engine_st ENGINE;
 typedef struct env_md_ctx_st EVP_MD_CTX;
 typedef struct env_md_st EVP_MD;
 typedef struct evp_aead_st EVP_AEAD;
+typedef struct evp_aead_ctx_st EVP_AEAD_CTX;
 typedef struct evp_cipher_ctx_st EVP_CIPHER_CTX;
 typedef struct evp_cipher_st EVP_CIPHER;
 typedef struct evp_encode_ctx_st EVP_ENCODE_CTX;
@@ -448,8 +449,6 @@ typedef struct trust_token_issuer_st TRUST_TOKEN_ISSUER;
 typedef struct trust_token_method_st TRUST_TOKEN_METHOD;
 typedef struct v3_ext_ctx X509V3_CTX;
 typedef struct x509_attributes_st X509_ATTRIBUTE;
-typedef struct x509_cert_aux_st X509_CERT_AUX;
-typedef struct x509_crl_method_st X509_CRL_METHOD;
 typedef struct x509_lookup_st X509_LOOKUP;
 typedef struct x509_lookup_method_st X509_LOOKUP_METHOD;
 typedef struct x509_object_st X509_OBJECT;
